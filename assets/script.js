5db3a0a96479b539fa3f5904f1fc3aa3e7eac5a1
ANDREWAHN-UK <ANDREW.AHN@HOTMAIL.CO.UK>
2021-04-30 15:31:16 +0000

    updated style.css; updated script.js

--- assets/script.js
+++ assets/script.js
@@ -3,26 +3,21 @@
 
 const icons = document.querySelectorAll('section-1-icons i')     /* allows selecting all icons */
 
-let i = 1 /* sets an increment counter to go through all the counters */
+let i=1
 
-setInterval(() =>{
+setInterval(() =>{  
 
-    i++ /* add 1 to the increment */
+    i++
 
     const icon = document.querySelector('.section-1-icons i.change');
     
-    icon.classList.remove('change');
+    icon.classList.remove('change');  
 
-    if (i < icons.length){
-
+    if (i <= icons.length) {
         icons[0].classList.add('change')
-
         i=1
-
-    } else {
-         icon.nextElementSibling.classList.add('change');
-    }
-
+    } else{
+        icon.nextElementSibling.classList.add('change') ;  
+    }   
    
-
-}, 4000) 
+}, 1000) 
