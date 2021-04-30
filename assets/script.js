58be49e3ab22e047edd5f69d187507a94ef8bd08
ANDREWAHN-UK <ANDREW.AHN@HOTMAIL.CO.UK>
2021-04-30 15:43:17 +0000

    modified script.js; modified style.css

--- assets/script.js
+++ assets/script.js
@@ -3,21 +3,13 @@
 
 const icons = document.querySelectorAll('section-1-icons i')     /* allows selecting all icons */
 
-let i=1
 
 setInterval(() =>{  
-
-    i++
 
     const icon = document.querySelector('.section-1-icons i.change');
     
     icon.classList.remove('change');  
-
-    if (i <= icons.length) {
-        icons[0].classList.add('change')
-        i=1
-    } else{
-        icon.nextElementSibling.classList.add('change') ;  
-    }   
+   
+    icon.nextElementSibling.classList.add('change') ;  
    
 }, 1000) 
