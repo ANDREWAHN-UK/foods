b17fd797cc1aacb2b6b694b376ed8a91ab16bcbc
ANDREWAHN-UK <ANDREW.AHN@HOTMAIL.CO.UK>
2021-05-01 09:52:47 +0000

    updated script.js

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,21 @@
 /* the code below displays the class 'change' and then removes it from an icon and places it on the next icon */
 
+const icons =document.querySelectorAll('.section-1-icons i')
 
-const icons = document.querySelectorAll('section-1-icons i')     /* allows selecting all icons */
+let i = 1
 
+setInterval(() =>{
+    i++
 
-setInterval(() =>{  
+    const icon = document.querySelector('.section-1-icons .change')
 
-    const icon = document.querySelector('.section-1-icons i.change');
-    
-    icon.classList.remove('change');  
-   
-    icon.nextElementSibling.classList.add('change') ;  
-   
-}, 4000) 
+    icon.classList.remove('change')
+
+    if (i >icons.length) {
+        icons[0].classList.add('change')
+        i = 1
+    } else{
+        icon.nextElementSibling.classList.add('change')
+    }
+
+}, 4000)
