5ccde0a7d2fa3a0df1ef2d3389a494d4c512a5d4
ANDREWAHN-UK <ANDREW.AHN@HOTMAIL.CO.UK>
2021-05-03 08:49:46 +0000

    created an onclick functioin in script.js to add .change affecting .target

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 /* the code below displays the class 'change' and then removes it from an icon and places it on the next icon */
-/* this code creates a basic slideshow of the designated icons, whihc are in section of the html */
+/* this code creates a basic slideshow of the designated icons, whihc are in section 1 of the html */
 
 const icons =document.querySelectorAll('.section-1-icons i')
 
@@ -20,3 +20,14 @@ setInterval(() =>{
     }
 
 }, 4000)
+
+
+/* this code affects the target class (navbar and menu) by adding effects when they are clicked, then removing them */
+
+document.querySelector('.menu').addEventListener('click', () => {
+
+    document.querySelectorAll('.target').forEach((item)=>{
+        item.classList.toggle('change')
+    })
+
+})
